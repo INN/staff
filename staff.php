815e92263c7a2ef43980294cf985e699241511ff
Ben Keith <ben@benlk.com>
2018-07-31 16:06:40 -0400

    initial commit

---
+++ staff.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Staff
+ * Plugin URI:      https://github.com/INN/staff
+ * Description:     Widget to list site staff, and associated helper functions
+ * Author:          INN Labs
+ * Author URI:      https://labs.inn.org
+ * Text Domain:     staff
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Staff
+ */
+
+// Your code starts here.
